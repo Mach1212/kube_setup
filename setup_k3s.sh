e21c3b6ed40fc27e15459fe77b9145008ae9377d
mach1212 <maciej.pruchnik@gmail.com>
2023-09-20 16:25:20 -0400

    [Feat + Refactor] Add bash-completion support and refactor script to be more friendly

--- setup_k3s.sh
+++ setup_k3s.sh
@@ -1,79 +1,85 @@
 #!/usr/bin/bash
 
-K3SUPS_EXE_PATH='/usr/local/bin/k3sup'
-
-if [ -f "$K3SUPS_EXE_PATH" ]; then
-	echo 'Skipping k3sup install...'
-else
-	gum confirm "Ok to install k3sup?" || exit
-	echo 'Downloading k3sup'
-
-	sudo curl -sLS https://get.k3sup.dev | sudo sh
-	if [ $? -ne 0 ]; then
-		printf "\nSomething unexpected occured during k3sup install...\n"
+handle_error() {
+	EXIT_STATUS=$?
+	if [ $EXIT_STATUS != 0 ]; then
+		echo $1: $EXIT_STATUS
 		exit
 	fi
-fi
-
-check_exit() {
+}
+handle_termination() {
 	if [ $? = 130 ]; then
 		exit
 	fi
 }
+final() {
+	gum confirm "Setup bash-completions?" && [[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] &&
+		. /usr/share/bash-completion/bash_completion
+	exit
+}
+main() {
+	gum filter <<<"random" &>/dev/null
+	handle_error "Download Charmbracelet Gum"
 
-while true; do
-	NODE_TYPE=$(gum choose --header "Setup node" --limit 1 "master" "worker") || exit
+	k3sup
+	if [ $? = 127 ]; then
+		echo 'Downloading k3sup'
 
-	MASTER_IP=$(gum input --header 'Master IP' --value "$MASTER_IP") || exit
-	MASTER_SSH_USER=$(gum input --header 'SSH User' --value "$MASTER_SSH_USER") || exit
-	if [ -z "$SSH_KEY_VALUE" ]; then
-		SSH_KEY_VALUE='~/.ssh/id_rsa'
+		if ! sudo curl -sLS https://get.k3sup.dev | sudo sh; then
+			printf "\nSomething unexpected occured during k3sup install...\n"
+			exit
+		fi
 	fi
-	SSH_KEY=$(gum input --header 'Private SSH Key' --value "$SSH_KEY_VALUE") || exit
+	kubectl
+	handle_error "Download kubectl"
 
-	if [ "$NODE_TYPE" = "master" ]; then
-		echo "Installing master..."
+	while true; do
+		NODE_TYPE=$(gum choose --header "Setup node" --limit 1 "master" "worker") || exit
+		MASTER_IP=$(gum input --header 'Master IP' --value "$MASTER_IP") || exit
+		MASTER_SSH_USER=$(gum input --header 'SSH User' --value "$MASTER_SSH_USER") || exit
+		if [ -z "$SSH_KEY_VALUE" ]; then
+			SSH_KEY_VALUE='~/.ssh/id_rsa'
+		fi
+		SSH_KEY=$(gum input --header 'Private SSH Key' --value "$SSH_KEY_VALUE") || exit
 
-		k3sup install \
-			--ip "$MASTER_IP" \
-			--user "$MASTER_SSH_USER" \
-			--ssh-key "$SSH_KEY" \
-			--sudo
+		if [ "$NODE_TYPE" = "master" ]; then
+			echo "Installing master..."
 
-		printf '\n** Kubernetes installed, everything else is optional**\n'
+			k3sup install \
+				--ip "$MASTER_IP" \
+				--user "$MASTER_SSH_USER" \
+				--ssh-key "$SSH_KEY" \
+				--sudo
+			handle_error "k3sup failed to install on $MASTER_IP"
 
-		gum confirm "Disable master's local firewall?" &&
-			gum spin ssh "$MASTER_SSH_USER"@"$MASTER_IP" -i "$SSH_KEY" "sudo systemctl stop firewalld && sudo systemctl disable firewalld"
-		check_exit
+			printf '\nKubernetes installed, everything else is optional'
+			printf 'Make sure to disable cloud firewall\n'
+			printf "\nEnsure port 6443 is open.\n"
 
-		test -f "$(pwd)/kubeconfig"
-		if [ $? = 1 ]; then
-			echo "Kubeconfig not in default directory. Unable to set environment."
+			if [ -f "$(pwd)/kubeconfig" ]; then
+				echo "Kubeconfig not in default directory. Unable to set environment."
+			else
+				export KUBECONFIG
+				KUBECONFIG="$(pwd)/kubeconfig"
+				gum confirm "Set KUBECONFIG in .bashrc?" &&
+					echo "export KUBECONFIG=\"$(pwd)/kubeconfig\"" >>~/.bashrc
+			fi
+
+			gum confirm "Alias kc to kubectl in .bashrc?" &&
+				echo "alias kc=kubectl" >>~/.bashrc &&
+				alias kc=kubectl
+		else
+			echo "Joining worker..."
+			k3sup join \
+				--ip "$(gum input --placeholder 'Worker node IP')" \
+				--user "$(gum input --placeholder 'Worker SSH User' --value "$MASTER_SSH_USER")" \
+				--server-ip "$MASTER_IP" \
+				--server-user "$MASTER_SSH_USER" \
+				--ssh-key "$SSH_KEY" \
+				--sudo
 		fi
 
-		gum confirm "Set KUBECONFIG in .bashrc?" &&
-			echo "export KUBECONFIG=\"$(pwd)/kubeconfig\"" >>~/.bashrc
-		check_exit
-		gum confirm "Alias kc to kubectl in .bashrc?" &&
-			echo "alias kc=kubectl" >>~/.bashrc &&
-			alias kc=kubectl
-		check_exit
-		export KUBECONFIG
-		KUBECONFIG="$(pwd)/kubeconfig"
-
-		printf "\nEnsure port 6443 is open, locally and in cloud!\n"
-		printf "Test cluster via \"kubectl get nodes\"\n"
-	else
-		echo "Joining worker..."
-		k3sup join \
-			--ip "$(gum input --placeholder 'Worker node IP')" \
-			--user "$(gum input --placeholder 'Worker SSH User' --value "$MASTER_SSH_USER")" \
-			--server-ip "$MASTER_IP" \
-			--server-user "$MASTER_SSH_USER" \
-			--ssh-key "$SSH_KEY" \
-			--sudo
-	fi
-
-	gum confirm 'Setup another node?' || exit
-
-done
+		gum confirm 'Setup another node?' || final
+	done
+}
+main
